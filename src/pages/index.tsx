9fdc97f9cbaa70baab37515a1a5a7aa2f5171197
Joy Shaheb <khondokoralam@gmail.com>
2023-10-09 23:31:19 +0600

    route pages added

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import Login from "./Login";
+import Signup from "./Signup";
+import ErrorPage from "./ErrorPage";
+
+export { ForgotPassword, Home, Login, ResetPassword, Signup, ErrorPage };
